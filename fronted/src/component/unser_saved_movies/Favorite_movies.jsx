3f2b1495c9691006a777fbe2dacfed1215273987
SondosEssam <sondosessam713@gmail.com>
2024-09-16 00:21:04 +0300

    user-data-3

---
+++ fronted/src/component/unser_saved_movies/Favorite_movies.jsx
@@ -0,0 +1,14 @@
+function Favorite_movies(){
+    return (
+        <div>
+        <h1>Favorite Movies</h1>
+        <p>Here are your favorite movies:</p>
+        <ul>
+            <li>Movie 1</li>
+            <li>Movie 2</li>
+            <li>Movie 3</li>
+        </ul>
+        </div>
+    );
+}
+export default Favorite_movies;
