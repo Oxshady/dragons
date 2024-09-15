0aeca175f680e32affbb120b2b1caba1d13dd11b
sayedabdelal <saidmo19734@gmail.com>
2024-09-16 00:23:45 +0200

    update pages

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -12,6 +12,13 @@ import RootPage from "./pages/RootPage.jsx";
 import ErrorRoute from "./UI/ErrorRoute.jsx";
 import HomePage from "./pages/home/HomePage.jsx";
 import MovieDetailPage,  { loader as BookLoader } from "./pages/MovieDetailPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SingupPage from "./pages/SingupPage.jsx";
+import Survey from "./component/survey/Survey.jsx";
+
+import Want_To_Watch from "./component/unser_saved_movies/Want_To_Watch.jsx";
+import Watched_before from "./component/unser_saved_movies/Watched_before.jsx";
+import Favorite_movies from "./component/unser_saved_movies/Favorite_movies.jsx";
 
 const router = createBrowserRouter([
   {
@@ -28,9 +35,34 @@ const router = createBrowserRouter([
         id: "movie-detail",
         element: <MovieDetailPage />,
         // loader: BookLoader
-      }
+      },
+      {
+        path:"survay",
+        element: <Survey/>
+      },
+      {
+        path: "want_to_watch",
+        element: <Want_To_Watch />,
+      },
+      {
+        path: "watched_before",
+        element: <Watched_before />,
+      },
+      {
+        path: "favorite_movies",
+        element: <Favorite_movies />,
+      },
     ]
+  },
+  {
+    path: "login",
+    element: <LoginPage />  
+  },
+  {
+    path: "register",
+    element: <SingupPage />
   }
+
 ]);
 function App() {
   
