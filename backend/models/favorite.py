1ac1b95f9d3bfc7851f22f73058116ef0e9841d2
Oxshady <shadymahmod55@gmail.com>
2024-09-16 17:22:38 +0300

    favorite

---
+++ backend/models/favorite.py
@@ -0,0 +1,22 @@
+from models.base_model import BaseModel, base
+from sqlalchemy import String, Integer, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
+from bcrypt import hashpw, gensalt,checkpw
+from time import time
+import jwt
+from flask import current_app
+from models import db
+class User(BaseModel, base):
+    """User class"""
+
+    __tablename__ = "users"
+    movie_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    rate: Mapped["int"] = mapped_column(Integer, nullable=False)
+    language: Mapped["str"] = mapped_column(String(30), nullable=False)
+    popularity: Mapped["float"] = mapped_column(Float, nullable=False)
+    user_id: Mapped["str"] = mapped_column(String(60), ForeignKey("users.id"), nullable=False)
+    
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize a new User object"""
+        super().__init__(*args, **kwargs)
