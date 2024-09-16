f00cbbb97c22a51bbdf85e58cfa393c395e126e1
Oxshady <shadymahmod55@gmail.com>
2024-09-16 07:21:15 +0300

    ^_^

--- backend/api/v1/views/__init__.py
+++ backend/api/v1/views/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 api_v1 = Blueprint('api_v1', __name__,url_prefix='/api/v1')
 
-from api.v1.views.radio import *
+from backend.api.v1.views.ai_recommend import *
 from api.v1.views.login import *
 from api.v1.views.register import *
 from api.v1.views.logout import *
