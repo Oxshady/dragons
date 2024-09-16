465f8fbee8393149a928af5092855becd1fd7495
Oxshady <shadymahmod55@gmail.com>
2024-09-16 05:45:58 +0300

    api init pack

---
+++ backend/api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+api_v1 = Blueprint('api_v1', __name__)
+
+from api.v1.views.radio import *
+from api.v1.views.login import *
+from api.v1.views.register import *
+from api.v1.views.logout import *
+from api.v1.views.reset_password import *
+from api.v1.views.profile import *
