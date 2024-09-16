25117c30556ec48236751027ee79a2d6cb097d4d
Oxshady <shadymahmod55@gmail.com>
2024-09-16 19:42:17 +0300

    watch list model ^_^

---
+++ backend/models/watch_list.py
@@ -0,0 +1,20 @@
+from models.base_model import BaseModel, base
+from sqlalchemy import String, Integer, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
+from time import time
+from flask import current_app
+from models import db
+class WatchList(BaseModel, base):
+    """watchlist class"""
+
+    __tablename__ = "watchlist"
+    movie_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    rate: Mapped["int"] = mapped_column(Integer, nullable=False)
+    language: Mapped["str"] = mapped_column(String(30), nullable=False)
+    popularity: Mapped["float"] = mapped_column(Float, nullable=False)
+    user_id: Mapped["str"] = mapped_column(String(60), ForeignKey("users.id"), nullable=False)
+    
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize a new User object"""
+        super().__init__(*args, **kwargs)
