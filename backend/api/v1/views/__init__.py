09aaffe128bc8029609ace433afd8e8cb630c514
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-09-16 12:56:17 +0300

    added smart_recommed endpoint

--- backend/api/v1/views/__init__.py
+++ backend/api/v1/views/__init__.py
@@ -2,9 +2,10 @@ from flask import Blueprint
 
 api_v1 = Blueprint('api_v1', __name__,url_prefix='/api/v1')
 
-from backend.api.v1.views.ai_recommend import *
-from api.v1.views.login import *
-from api.v1.views.register import *
-from api.v1.views.logout import *
-from api.v1.views.reset_password import *
-from api.v1.views.profile import *
+from api.v1.views.ai_recommend import *
+# from api.v1.views.login import *
+# from api.v1.views.register import *
+# from api.v1.views.logout import *
+# from api.v1.views.reset_password import *
+# from api.v1.views.profile import *
+from api.v1.views.smart_recommend import *
--- backend/api/v1/views/smart_recommend.py
+++ backend/api/v1/views/smart_recommend.py
@@ -6,16 +6,8 @@ from dotenv import load_dotenv
 from os import getenv
 load_dotenv()
 history = []
-questions_answers ={
-"What is your preferred movie genre?":"Action",
-"In which language do you prefer watching movies?":"English",
-"What time period of movies do you enjoy the most?":"2020s",
-"What kind of movie pace do you prefer?":"Fast-paced and action-packed",
-"What is your favorite type of storytelling?":"Character-driven",
-"What type of movie endings do you prefer?":"open endings"
-}
-
-def ai_model(questions_answers):
+movies_watched = ["Inception", "The Matrix", "Mad Max: Fury Road", "John Wick", "The Dark Knight"]
+def ai_model(movies_watched):
     """
     ai model
     """
@@ -35,7 +27,7 @@ def ai_model(questions_answers):
     model = genai.GenerativeModel(
         model_name="gemini-1.5-flash",
         generation_config=generation_config,
-        system_instruction="You are a highly intelligent engine designed to analyze movie preferences based on user inputs. The input will be a dictionary consisting of 6 keys, each representing a question, and their corresponding answers, which reflect the user's movie-watching preferences (e.g., genre, language, time period, storytelling style, etc.). Your task is to creatively analyze the input and recommend up to 5 film titles that best match the user's preferences.\n\nThe output should be a list of film titles (just the names of the films) that the user can later use to search for more details on MovieDB. Ensure that the recommended films align with the given preferences, drawing from a wide range of movie genres, languages, and time periods.\n\n",
+        system_instruction="You are a highly intelligent engine designed to analyze movie preferences based on user inputs. The input will be a list of movie names that the user liked. Your task is to creatively analyze the input and recommend up to 5 film titles that best match the user's preferences.\n\nThe output should be a list of film titles (just the names of the films) that the user can later use to search for more details on MovieDB. Ensure that the recommended films align with the given preferences, drawing from a wide range of movie genres, languages, and time periods.\n\n",
     )
 
     chat_session = model.start_chat(
@@ -45,7 +37,7 @@ def ai_model(questions_answers):
     response = chat_session.send_message({
         'parts': [
             {
-                'text': json.dumps(questions_answers)
+                'text': json.dumps(movies_watched)
             }
         ]
     }).to_dict()
@@ -54,7 +46,7 @@ def ai_model(questions_answers):
     {
         "role": "user",
         "parts": [
-        json.dumps(questions_answers),
+        json.dumps(movies_watched),
         ],
     }
     )
@@ -91,13 +83,12 @@ def movies(recommendations):
 
 
 
-@api_v1.route('/films', methods=['GET'], strict_slashes=False)
-def films():
-    data = questions_answers
+@api_v1.route('/films/smart-recommend', methods=['GET'], strict_slashes=False)
+def films_smart():
+    data = movies_watched
     if data is None:
         return jsonify({'error': 'No data provided'}), 400
     resp = ai_model(data)
     rec = movies(resp)
     return jsonify(rec), 200
-    
     
